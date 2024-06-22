234adaa11e857eb9c46563e1ac75b39b32eeb96b
Resimlrr1912 <lavbinmr@gmail.com>
2024-06-23 02:06:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "", 
-    prefix: ".",
+    token: "MTI1NDE3MDQ2NTc2NTk1MzU4Nw.G4oIwr.cTtjzM7Ynb_BJphUlT5wx0BPB67EkzGuRDsp08", 
+    prefix: "Lm!",
     destek: "",
     topgg: "",
     davet: "",
