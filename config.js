5a20f35f1152431ea824a38b00218a21148ce0f5
Resimlrr1912 <lavbinmr@gmail.com>
2024-06-23 02:27:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "MTI1NDIxMzU1OTE4MTkwNTk1MA.GK1qfZ._hlASJNMVh_iNupfHQK4x3PIQHnizWE_2FC2fg", 
+    token: "MTI1NDIxMzU1OTE4MTkwNTk1MA.GEeAd5.kMG81fzMMfS0IPpy6D0Z8T4VFKAHL-fKWIdX4g", 
     prefix: "mr!",
     destek: "",
     topgg: "",
